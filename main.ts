16a305ae8f7fed3533c7de4d8a7f6ed6631e2413
0jaaack <jack@hopsoffice.com>
2025-01-11 18:44:24 +0900

    도서관 도서 추가 기능 구현

--- main.ts
+++ main.ts
@@ -4,6 +4,8 @@ type libraryData = any;
 type UserManagementData = any;
 // 나중에 구현될 예정
 type CatalogData = any;
+// 나중에 구현될 예정
+type BookItemInfo = any;
 
 class Library {
   static getBookLendings(libraryData: libraryData, userId: string, memberId: string) {
@@ -14,6 +16,17 @@ class Library {
       return Catalog.getBookLendings(libraryData.catalog, memberId);
     } else {
       throw "대출된 도서를 조회할 권한이 없음";
+    }
+  }
+
+  static addBookItem(libraryData: libraryData, userId: string, bookItemInfo: BookItemInfo) {
+    if (
+      UserManagement.isLibrarian(libraryData.userManagement, userId)
+      || UserManagement.isVIPMember(libraryData.userManagement, userId)
+    ) {
+      Catalog.addBookItem(libraryData.catalog, bookItemInfo);
+    } else {
+      throw "도서 추가 권한이 없음";
     }
   }
 }
@@ -28,10 +41,19 @@ class UserManagement {
     // 나중에 구현될 예정
     return false;
   }
+
+  static isVIPMember(userManagementData: UserManagementData, userId: string): boolean {
+    // 나중에 구현될 예정
+    return false;
+  }
 }
 
 class Catalog {
   static getBookLendings(catalogData: CatalogData, memberId: string) {
+    // 나중에 구현될 예정
+  }
+
+  static addBookItem(catalogData: CatalogData, bookItemInfo: BookItemInfo) {
     // 나중에 구현될 예정
   }
 }
