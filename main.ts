c51372393f1922b71d33347de0081462d7f7d330
0jaaack <jack@hopsoffice.com>
2025-01-11 18:39:10 +0900

    대출된 도서 목록 조회기능 구현

--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
-console.log("Hello, World!");
+// 나중에 구현될 예정
+type libraryData = any;
+// 나중에 구현될 예정
+type UserManagementData = any;
+// 나중에 구현될 예정
+type CatalogData = any;
+
+class Library {
+  static getBookLendings(libraryData: libraryData, userId: string, memberId: string) {
+    if (UserManagement.isLibrarian(libraryData.userManagement, userId)) {
+      return Catalog.getBookLendings(libraryData.catalog, memberId);
+    } else {
+      throw "대출된 도서를 조회할 권한이 없음";
+    }
+  }
+}
+
+class UserManagement {
+  static isLibrarian(userManagementData: UserManagementData, userId: string): boolean {
+    // 나중에 구현될 예정
+    return false;
+  }
+}
+
+class Catalog {
+  static getBookLendings(catalogData: CatalogData, memberId: string) {
+    // 나중에 구현될 예정
+  }
+}
